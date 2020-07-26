e62eaaa2cd32f1db964b30926d58ede35b4920e6
Erkan IŞIK <erkanisik@yahoo.com>
2020-07-26 17:51:20 +0300

    ,

--- Projects/Frontend/Controllers/Home.php
+++ Projects/Frontend/Controllers/Home.php
@@ -3,28 +3,14 @@ use Import, DB, ML;
 
 class Home extends Controller
 {
-    /**
-     * Home::main
-     * 
-     * Loads opening page.
-     * Location: Views/Home/main.wizard.php
-     */
-    public function main(String ...$parameters)
-    {  
+   
+    public function main(String ...$parameters){  
         
         View::homeContent(DB::orderBy('id','desc')->limit('5')->contentResult())
             ::get('main');
-       
     } 
 
-    /**
-     * Home::s404
-     * 
-     * Loads show 404 page.
-     * Location: Views/Home/s404.wizard.php
-     */
-    public function s404()
-    {
+    public function s404(){
         # Sets masterpage title.
         Masterpage::title('404! File Not Found');
     }
